72c62c788b4e754d4e048a8b548975a93e5aa04d
Golovin Savelii <Golovin.savelii@yandex.ru>
2025-09-15 13:55:12 +0500

    Разработанный метод UserInfoPlayer. Сабитов.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace Практическая_работа_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Данные игрока
+        /// </summary>
+        public Classes.PersonInfo Player = new Classes.PersonInfo("Student", 100, 10, 1, 0, 0, 5);
         public MainWindow()
         {
             InitializeComponent();
+            // Повышаем уровень персонажа и обновляем данные на UI
+            UserInfoPlayer();
+        }
+        /// <summary>
+        /// Повышение уровня и обновление данных на UI
+        /// </summary>
+        public void UserInfoPlayer()
+        {
+            if(Player.Glasses > 100 * Player.Level)
+            {
+                Player.Level++;
+
+                Player.Glasses = 0;
+
+                Player.Health += 100;
+
+                Player.Damage++;
+
+                Player.Armor++;
+            }
+            playerHealth.Content = "Жизненные показатели: " + Player.Health;
+            playerArmor.Content = "Броня: " + Player.Armor;
+            playerLevel.Content = "Уровень: " + Player.Level;
+            playerGlasses.Content = "Опыт: " + Player.Glasses;
+            playerMoney.Content = "Монеты: " + Player.Money;
         }
     }
 }
