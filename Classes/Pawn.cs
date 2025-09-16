19d17914927b12fa50ec17c67295e3d656639668
Назир Сабитов <beta9524@gmail.com>
2025-09-16 17:44:18 +0500

    Программирование окна MainWindow. Сабитов

--- Classes/Pawn.cs
+++ Classes/Pawn.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -10,12 +6,11 @@ using System.Windows.Media.Imaging;
 
 namespace Chess_Сабитов2.Classes
 {
-    internal class Pawn
+    public class Pawn
     {
         public int X { get; set; }
         public int Y { get; set; }
-        public bool Select = false;
-        public bool Black = false;
+        public bool Select, Black;
         public Grid Figure { get; set; }
         public Pawn(int X, int Y, bool Black)
         {
@@ -25,45 +20,42 @@ namespace Chess_Сабитов2.Classes
         }
         public void SelectFigure(object sender, MouseButtonEventArgs e)
         {
-            bool atack = false;
-            Pawn SelectPawn = MainWindow.mainWindow.Pawns.Find(x => x.Select == true);
-            //Если выделенная пешка существует
+            Pawn SelectPawn = MainWindow.init.Pawns.Find(x => x.Select == true);
             if (SelectPawn != null)
             {
-                if (this.Black && this.Y - 1 == SelectPawn.Y && (this.X - 1 == SelectPawn.X || this.X == SelectPawn.X || this.X + 1 == SelectPawn.X) ||
-                    !this.Black && this.Y + 1 == SelectPawn.Y && (this.X - 1 == SelectPawn.X || this.X == SelectPawn.X || this.X + 1 == SelectPawn.X))
+                if ((SelectPawn.Black && Y - 1 == SelectPawn.Y && (X == SelectPawn.X - 1 || X == SelectPawn.X + 1)) || 
+                    (SelectPawn.Black == false && Y + 1 == SelectPawn.Y && (X == SelectPawn.X - 1 || X == SelectPawn.X + 1)))
                 {
-                    MainWindow.mainWindow.gameBoard.Children.Remove(this.Figure);
-                    Grid.SetColumn(SelectPawn.Figure,this.X);
-                    Grid.SetRow(SelectPawn.Figure,this.Y);
-                    SelectPawn.X = this.X;
-                    SelectPawn.Y = this.Y;
-                    SelectPawn.SelectFigure(null,null);
-                    atack = true;
+                    MainWindow.init.gameBoard.Children.Remove(Figure);
+                    Grid.SetColumn(SelectPawn.Figure, X);
+                    Grid.SetRow(SelectPawn.Figure, Y);
+
+                    SelectPawn.X = X;
+                    SelectPawn.Y = Y;
+
+                    SelectPawn.SelectFigure(null, null);
+                    return;
                 }
             }
 
-            // Если атака не была произведена, значит нам необходимо выделить пешку
-            if(!atack)
+            MainWindow.init.OnSelect(this);
+
+            if (Select)
             {
-                MainWindow.mainWindow.OnSelect(this);
-                if (this.Select)
+                if (Black)
                 {
-                    if (this.Black)
-                    {
-                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Image/Pawn (black).png")));
-                    }
-                    else
-                    {
-                        this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Image/Pawn.png")));
-                    }
-                    this.Select = false;
+                    Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (black).png")));
                 }
+                else
+                {
+                    Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                }
+                Select = false;
             }
             else
             {
-                this.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Image/Pawn (select).png")));
-                this.Select = true;
+                Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (select).png")));
+                Select = true;
             }
         }
         public void Transform(int X, int Y)
@@ -73,14 +65,15 @@ namespace Chess_Сабитов2.Classes
                 SelectFigure(null,null);
                 return;
             }
-            if(!Black && ((this.Y == 1 && this.Y + 2 == Y) || this.Y + 1 == Y) ||
-                Black && ((this.Y == 6 && this.Y + 2 == Y) || this.Y + 1 == Y))
+            if(Black && ((this.Y == 1 && this.Y + 2 == Y) || this.Y + 1 == Y) ||
+                !Black && ((this.Y == 6 && this.Y - 2 == Y) || this.Y - 1 == Y))
             {
-                Grid.SetColumn(this.Figure, X);
-                Grid.SetRow(this.Figure, Y);
+                Grid.SetColumn(Figure, X);
+                Grid.SetRow(Figure, Y);
                 this.X = X;
                 this.Y = Y;
-            }
+            } 
+
             SelectFigure(null,null);
         }
     }
