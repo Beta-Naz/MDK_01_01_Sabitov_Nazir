22ed9637a8717e073565ad90a29e70554190611c
Sabitov <sabitovnazir6@gmail.com>
2025-09-22 10:11:13 +0500

    Разработанный класс Student. Сабитов

---
+++ Classes/Stutend.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Сабитов.Classes
+{
+    public class Student
+    {
+        public string Firstname = "", Lastname = "", Surname = "";
+        public bool Scholarship = false;
+        public int Course = 4;
+
+        public Student(string firstname, string lastname, string surname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Surname = surname;
+        }
+        public Student(string firstname, string lastname, string surname, bool scholarship)
+            : this(firstname, lastname, surname) 
+            {
+                Scholarship = scholarship;
+        }
+
+        public Student(string firstname, string lastname, string surname, bool scholarship, int course)
+            : this(firstname, lastname, surname, scholarship)
+        {
+            Course = course;
+        }
+        public string GetFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
