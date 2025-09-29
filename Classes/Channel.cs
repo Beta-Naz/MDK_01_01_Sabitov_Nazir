bb0029683718338a855fb47392e1b5a3a5a0b718
ANosok <kirill60278@gmail.com>
2025-09-29 09:27:31 +0500

    Разработанный класс Channel. Сабитов

---
+++ Classes/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Сабитов.Classes
+{
+    internal class Channel
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
