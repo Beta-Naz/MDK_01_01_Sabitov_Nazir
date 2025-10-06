622167ba499ca4ed1760fba5a97774039a575407
Kik02 <Golovin.Savelii@yandex.ru>
2025-10-06 10:17:46 +0500

    Разработанный класс Children. Сабитов

---
+++ Classes/Children.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Сабитов.Classes
+{
+    public class Children : Shop
+    {
+        public int Age { get; set; }
+
+        public Children(string Name, int Price, int Age) : base(Name, Price)
+        {
+            this.Age = Age;
+        }
+    }
+}
