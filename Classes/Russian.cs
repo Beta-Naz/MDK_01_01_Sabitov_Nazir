4c334e939c55f3588ea465dcbb589d4facbc4af6
Shashin Alexey <alexshashin.2019@gmail.com>
2025-10-13 16:27:59 +0500

    Сгенерированные фразы. Сабитов.

---
+++ Classes/Russian.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Сабитов.Classes
+{
+    public class Russian : Human
+    {
+        private List<Phrase> Phrases {  get; set; }
+        private int stepAudio;
+
+        private int StepAudio
+        {
+            get { return stepAudio; }
+            set {stepAudio = value;
+                if (stepAudio > Phrases.Count - 1)
+                {
+                    stepAudio = 0;
+                }
+            }
+        }
+        public Russian(string name, string img) : base(name, img)
+        {
+            Phrases = AllPhrases();
+        }
+        public override void Speak(Label Phrase)
+        {
+            Phrase.Content = Phrases[StepAudio]._Phrase;
+            MainWindow.MediaPLayer.Open(new Uri(Phrases[StepAudio].Src));
+            MainWindow.MediaPLayer.Play();
+            StepAudio++;
+        }
+        public static List<Phrase> AllPhrases()
+        {
+            List<Phrase> allPhrases = new List<Phrase>()
+            {
+                new Phrase("Привет", @"C:\Users\student-a502\Desktop\Human_Сабитов\Voices\Привет.mp3"),
+                new Phrase("Как дела", @"C:\Users\student-a502\Desktop\Human_Сабитов\Voices\Как_дела.mp3"),
+                new Phrase("Меня зовут Александр", @"C:\Users\student-a502\Desktop\Human_Сабитов\Voices\Меня_зовут_Александр.mp3")
+            };
+            return allPhrases;
+        }
+    }
+}
