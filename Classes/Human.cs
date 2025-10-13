82a55711ae185d77ce6d1ffce14ee1ddc7ceabaf
Shashin Alexey <alexshashin.2019@gmail.com>
2025-10-13 15:56:37 +0500

    Разработанный класс «Human». Сабитов.

---
+++ Classes/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human_Сабитов.Classes
+{
+    public abstract class Human
+    {
+        public string Name { get; set; }
+        public string Img { get; set; }
+        public Human(string name, string img)
+        {
+            Name = name;
+            Img = img;
+        }
+        public abstract void Speak(Label Phrase);
+    }
+}
