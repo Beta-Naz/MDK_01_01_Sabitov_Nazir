14c012a8657d6ccf52aff128c93abcbb73075526
kik02 <Golovin.Savelii@yandex.ru>
2025-10-20 17:40:57 +0500

    Создание класса Genre. Сабитов

---
+++ Classes/Genre.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Book_Сабитов.Classes
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Genre(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public static List<Genre> AllGenres()
+        {
+            List<Genre> allGenres = new List<Genre>()
+            {
+                new Genre(1, "Современная русская литература"),
+                new Genre(2, "Современные детективы"),
+                new Genre(3, "Любовное фентэзи"),
+                new Genre(4, "Исекай"),
+                new Genre(5, "Юмористическое фэнтези"),
+            };
+
+            return allGenres;
+        }
+    }
+}
