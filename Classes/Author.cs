60ac68ba4de5aac755827b0cb3530b1b90e8e7e7
kik02 <Golovin.Savelii@yandex.ru>
2025-10-20 17:45:39 +0500

    Создание класса Author. Сабитов

---
+++ Classes/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Сабитов.Classes
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Author(int id, string fIO)
+        {
+            Id = id;
+            FIO = fIO;
+        }
+        public static List<Author> AllAuthors()
+        {
+            List<Author> allAuthors = new List<Author>()
+            {
+                new Author(1, "Виктор Пелевин"),
+                new Author(2, "Александр Маринина"),
+                new Author(3, "Ольга Герр"),
+            };
+            return allAuthors;
+        }
+    }
+}
