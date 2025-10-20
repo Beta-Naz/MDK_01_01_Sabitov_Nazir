ba26962ada4d26ed2ab5a3c175a4ed40cd453501
kik02 <Golovin.Savelii@yandex.ru>
2025-10-20 18:11:29 +0500

    Создание пользовательского элемента. Сабитов

---
+++ Elements/Element.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Book_Сабитов.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Element.xaml
+    /// </summary>
+    public partial class Element : UserControl
+    {
+        public Element(Classes.Book book)
+        {
+            InitializeComponent();
+            tbName.Text = $"Наименование: {book.Name} ({book.Year} г.)";
+            tbGenre.Text = $"Жанр: {book.ToGenres()}";
+            tbAuthor.Text = $"Автор: {book.ToAuthors()}";
+        }
+    }
+}
