ecbbfdb273bec47b77bd091d475262f0bdbca43f
Назир Сабитов <beta9524@gmail.com>
2025-10-27 17:47:18 +0500

    Реализованные модели. Сабитов

---
+++ Interface_Сабитов/Models/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Сабитов.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create {  get; set; }
+        public int IdUsers { get; set; }
+        public Messages()
+        {
+
+        }
+        public Messages(string message, DateTime create, int idUsers)
+        {
+            Message = message;
+            Create = create;
+            IdUsers = idUsers;
+        }
+    }
+}
---
+++ Interface_Сабитов/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Сабитов.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Users() { }
+        public Users(int id, string fIO)
+        {
+            Id = id;
+            FIO = fIO;
+        }
+    }
+}
