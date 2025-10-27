7c47faeed46cac92bed8a3df43e896404ed66a3c
Назир Сабитов <beta9524@gmail.com>
2025-10-27 17:53:38 +0500

    Реализованные интерфейсы. Сабитов

---
+++ Interface_Сабитов/Interfaces/IMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interface_Сабитов.Interfaces
+{
+    public interface IMessages
+    {
+        void All(out List<IMessages> Messages);
+        void Save (bool Update = false);
+        void Delete();
+    }
+}
---
+++ Interface_Сабитов/Interfaces/IUsers.cs
@@ -0,0 +1,10 @@
+﻿using Interface_Сабитов.Models;
+using System.Collections.Generic;
+
+namespace Interface_Сабитов.Interfaces
+{
+    public interface IUsers
+    {
+        void All(out List<Users> Users);
+    }
+}
