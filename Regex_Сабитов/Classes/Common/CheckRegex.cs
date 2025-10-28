930a96a03fc5df3de943bc6175f64a61a3fcaaad
Назир Сабитов <beta9524@gmail.com>
2025-10-28 23:22:18 +0500

    Создание классов. Сабитов

---
+++ Regex_Сабитов/Classes/Common/CheckRegex.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regex_Сабитов.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string pattern, string input)
+        {
+            Match m = Regex.Match(pattern, input);
+            return m.Success;
+        }
+    }
+}
---
+++ Regex_Сабитов/Classes/Passport.cs
@@ -0,0 +1,15 @@
+﻿namespace Regex_Сабитов.Classes
+{
+    public class Passport
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string Forename { get; set; }
+        public string Ussued { get; set; }
+        public string DateOfUssued { get; set; }
+        public string DepartmentCode { get; set; }
+        public string SeriesAndNumber {get; set;}
+        public string DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+    }
+}
