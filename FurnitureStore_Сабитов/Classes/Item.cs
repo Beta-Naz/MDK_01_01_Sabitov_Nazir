775758a6dc06543d34600b02e9add7d3d51cda43
Назир Сабитов <beta9524@gmail.com>
2025-11-23 20:04:07 +0500

    Разработанный класс. Сабитов

---
+++ FurnitureStore_Сабитов/Classes/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace FurnitureStore_Сабитов.Classes
+{
+    public class Item
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public string src { get; set; }
+        public Item(string name, int price, string src)
+        {
+            this.name = name;
+            this.price = price;
+            this.src = src;
+        }
+    }
+}
