1a5fef62db1d0603580c26c21d5a4921b6631917
Назир Сабитов <beta9524@gmail.com>
2025-11-23 19:29:18 +0500

    Начало. Сабитов

--- FurnitureStore_Сабитов/MainWindow.xaml.cs
+++ FurnitureStore_Сабитов/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace FurnitureStore_Сабитов
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {  
         public MainWindow()
         {
             InitializeComponent();
