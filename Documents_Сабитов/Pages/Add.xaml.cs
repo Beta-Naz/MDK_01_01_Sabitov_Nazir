12010071e790aa1bdb10cba406ad23675d7a9d10
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-24 20:08:24 +0500

    Стоп №1. Сабитов

--- Documents_Сабитов/Pages/Add.xaml.cs
+++ Documents_Сабитов/Pages/Add.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Documents_Сабитов.Classes;
+using Microsoft.Win32;
 
 namespace Documents_Сабитов.Pages
 {
@@ -21,9 +22,67 @@ namespace Documents_Сабитов.Pages
     /// </summary>
     public partial class Add : Page
     {
+        string s_src = "";
         public Add(DocumentContext document = null)
         {
             InitializeComponent();
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.main);
+        }
+
+        private void SelectImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "PNG (*.png)|*.png|All files(*.*)|*.*";
+            openFileDialog.ShowDialog();
+            if(openFileDialog.FileName != "")
+            {
+                src.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                s_src = openFileDialog.FileName;
+            }
+        }
+
+        private void AddDocument(object sender, RoutedEventArgs e)
+        {
+            if(s_src.Length == 0)
+            {
+                MessageBox.Show("Необходимо выберать изображение");
+                return;
+            }
+            if (tbName.Text.Length == 0)
+            {
+                MessageBox.Show("Необходимо указать наименование");
+                return;
+            }
+            if (tbUser.Text.Length == 0)
+            {
+                MessageBox.Show("Необходимо указать ответственного");
+                return;
+            }
+            if (tbDocumentCode.Text.Length == 0)
+            {
+                MessageBox.Show("Необходимо указать код документа");
+                return;
+            }
+            if (tbDate.Text.Length == 0)
+            {
+                MessageBox.Show("Необходимо указать дату поступления");
+                return;
+            }
+            if (tbStatus.SelectedIndex == -1)
+            {
+                MessageBox.Show("Необходимо выбрать указать статус документа");
+                return;
+            }
+            if (tbDirection.Text.Length == 0)
+            {
+                MessageBox.Show("Необходимо указать направление");
+                return;
+            }
         }
     }
 }
