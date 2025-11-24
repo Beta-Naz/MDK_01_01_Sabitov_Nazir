c8fa139ca98dd3c3c81146f0f14456ee3ba3637d
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-24 18:37:07 +0500

    Реализованные классы. Сабитов

---
+++ Documents_Сабитов/Classes/Common/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.OleDb;
+
+namespace Documents_Сабитов.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string Path = @"C:\\Users\\student-a502.PERMAVIAT\\Desktop\\4пр\\Documents_Сабитов\\Documents_Сабитов\\bin\\Debug\\DataBase.accdb";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            conn.Open();
+            return conn;
+        }
+        public static OleDbDataReader Query(string Query, OleDbConnection connection)
+        {
+            return new OleDbCommand(Query, connection).ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ Documents_Сабитов/Classes/DocumentContext.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Documents_Сабитов.Classes.Common;
+using Documents_Сабитов.Interfaces;
+using Documents_Сабитов.Model;
+
+namespace Documents_Сабитов.Classes
+{
+    public class DocumentContext : Document, IDocument
+    {
+        public List<Document> AllDocument()
+        {
+            List<Document> allDocument = new List<Document>();
+            OleDbConnection connection = DBConnection.Connection();
+            OleDbDataReader dataDocuments = DBConnection.Query("SELECT * FROM [Документы]", connection);
+            while (dataDocuments.Read())
+            {
+                allDocument.Add(new Document()
+                {
+                    Id = dataDocuments.GetInt32(0),
+                    Src = dataDocuments.GetString(1),
+                    Name = dataDocuments.GetString(2),
+                    User = dataDocuments.GetString(3),
+                    IdDocument = dataDocuments.GetInt32(4),
+                    Date = dataDocuments.GetDateTime(5),
+                    Status = dataDocuments.GetInt32(6),
+                    Direction = dataDocuments.GetInt32(7),
+                });
+            }
+            DBConnection.CloseConnection(connection);
+            return allDocument;
+        }
+
+        public void Delete()
+        {
+            OleDbConnection connection = Common.DBConnection.Connection();
+            Common.DBConnection.Query($"DELETE FROM [Документы] WHERE [Код] = {this.Id}", connection);
+            Common.DBConnection.CloseConnection(connection);
+        }
+
+        public void Save(bool update = false)
+        {
+            OleDbConnection connection = DBConnection.Connection();
+            if (update)
+            {
+                DBConnection.Query(
+                    $"UPDATE " +
+                        $"[Документы] " +
+                    $"SET " +
+                        $"[Изображение] = '{this.Src}', " +
+                        $"[Наименование] = '{this.Name}', " +
+                        $"[Ответственный] = '{this.User}', " +
+                        $"[Код документа] = '{this.IdDocument}', " +
+                        $"[Дата поступления] = '{this.Date.ToString("dd.MM.yyyy")}', " +
+                        $"[Статус] = '{this.Status}', " +
+                        $"[Направление] = '{this.Direction}', " +
+                    $"WHERE " +
+                        $"[Код]= {this.Id}", connection);
+            }
+            else
+            {
+                DBConnection.Query(
+                        $"INSERT INTO " +
+                         $"[Документы](" +
+                             $"[Изображение], " +
+                             $"[Наименование], " +
+                             $"[Ответственный], " + 
+                             $"[Код документа], " +
+                             $"[Дата поступления], " +
+                             $"[Статус], [Направление]) " +
+                        $"VALUES " +
+                           $"('{this.Src}', " +
+                           $"'{this.Name}', " +
+                           $"'{this.User}', " +
+                           $"'{this.IdDocument}', " +
+                           $"'{this.Date.ToString("dd.MM.yyyy")}', " +
+                           $"'{this.Status}', " +
+                           $"'{this.Direction}',)", connection);
+            }
+        }
+    }
+}
