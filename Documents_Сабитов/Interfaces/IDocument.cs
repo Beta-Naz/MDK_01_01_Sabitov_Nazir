2f72e640e107502dc5d1893ecb0fcdb3111648b5
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-24 17:49:31 +0500

    Реализованные интерфейсы. Сабитов

---
+++ Documents_Сабитов/Interfaces/IDocument.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+namespace Documents_Сабитов.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool update = false);
+        List<Model.Document> AllDocument();
+        void Delete();
+    }
+}
