854cdb35bac0da3674e72b9515b2126ecd8ef90a
Назир Сабитов <beta9524@gmail.com>
2025-12-14 19:38:55 +0500

    Разработанные классы. Сабитов.

--- ClassModule/Call.cs
+++ ClassModule/Call.cs
@@ -5,7 +5,7 @@
         public int id {  get; set; }
         public int user_id { get; set; }
         public int category_call { get; set; }
-        public string date { get; set; }
+        public string date { get; set; } 
         public string time_start { get; set; }
         public string time_end { get; set; }
     }
