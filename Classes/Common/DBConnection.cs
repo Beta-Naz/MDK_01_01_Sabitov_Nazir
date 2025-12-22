8ad881216d3bde2f04cd827bbe591586713e894d
ANosok <kirill60278@gmail/com>
2025-12-22 18:32:54 +0500

    Разработка общего класса. Сабитов.

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.OleDb;
+
+namespace Shop_Сабитов.Classes.Common
+{
+    internal class DBConnection
+    {
+        public static readonly string Path = "";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            connection.Open();
+            return connection;
+        }
+        public static OleDbDataReader Query(string query, OleDbConnection connection)
+        {
+            return new OleDbCommand(query, connection).ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
