5a6754aa43da1a1317ce3761914b522254349b7f
Назир Сабитов <beta9524@gmail.com>
2026-01-12 19:45:35 +0500

    Разработана страница RedactorCinema. Сабитов

---
+++ Сinema_Сабитов/Pages/RedactorCinema.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+
+namespace Сinema_Сабитов.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RedactorCinema.xaml
+    /// </summary>
+    public partial class RedactorCinema : Page
+    {
+        MainWindow mainWindow;
+        public RedactorCinema(MainWindow window)
+        {
+            InitializeComponent();
+            mainWindow = window;
+        }
+
+        private void SearchPanel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void ClearFilter(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+        private void ApplyFilter(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Сinema_Сабитов/Pages/RedactorPoster.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сinema_Сабитов.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RedactorPoster.xaml
+    /// </summary>
+    public partial class RedactorPoster : Page
+    {
+        public RedactorPoster()
+        {
+            InitializeComponent();
+        }
+    }
+}
