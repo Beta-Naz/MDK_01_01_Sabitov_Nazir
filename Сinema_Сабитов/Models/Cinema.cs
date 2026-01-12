06a7ec66bfdf5942f077efae1f0641e4d5da2b24
Назир Сабитов <beta9524@gmail.com>
2026-01-12 18:14:56 +0500

    Добавьте файлы проекта.

---
+++ Сinema_Сабитов/Models/Cinema.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сinema_Сабитов.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public int CountHall { get; set; }
+        public int CountPlace { get; set; }
+        public Cinema() { }
+        public Cinema(int id, int countHall, int countPlace)
+        {
+            Id = id;
+            CountHall = countHall;
+            CountPlace = countPlace;
+        }
+    }
+}
---
+++ Сinema_Сабитов/Models/Poster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сinema_Сабитов.Models
+{
+    public class Poster
+    {
+        public int Id { get; set; }
+        public string IdCinema { get; set; }
+        public string Film { get; set; }
+        public TimeSpan TimeSession;
+        public decimal PriceSession;
+        public decimal PriceTicket;
+        public Poster() { }
+        public Poster(int id, string idCinema, string film, TimeSpan timeSession, decimal priceSession, decimal priceTicket)
+        {
+            Id = id;
+            IdCinema = idCinema;
+            Film = film;
+            TimeSession = timeSession;
+            PriceSession = priceSession;
+            PriceTicket = priceTicket;
+        }
+    }
+}
