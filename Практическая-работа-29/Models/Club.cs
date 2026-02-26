7f91f70c9c3e0b312d7a993877ef4614c4741eac
Beta-Naz <sabitovnazir6@gmail.com>
2026-02-26 09:14:10 +0500

    Разработанные модели. Сабитов

---
+++ Практическая-работа-29/Models/Club.cs
@@ -0,0 +1,10 @@
+﻿namespace Практическая_работа_29.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string WorkTime { get; set; }
+    }
+}
---
+++ Практическая-работа-29/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Практическая_работа_29.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub {  get; set; }
+    }
+}
