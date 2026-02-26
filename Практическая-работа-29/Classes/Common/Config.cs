4a3e6605f2ddb074ff3bdc37499a1e9f0881fe2a
Beta-Naz <sabitovnazir6@gmail.com>
2026-02-26 09:26:56 +0500

    Разработанные контексты для моделей. Сабитов

---
+++ Практическая-работа-29/Classes/Common/Config.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Практическая_работа_29.Classes.Common
+{
+    public class Config
+    {
+        public static string Login = "root";
+        public static string Password = "1234";
+        public static readonly string ConnectionConfig = $"server=10.0.201.4;uid={Login};pwd={Password};database=pcClub";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8,0,11));
+    }
+}
---
+++ Практическая-работа-29/Classes/Context/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Практическая_работа_29.Classes.Common;
+using Практическая_работа_29.Models;
+
+namespace Практическая_работа_29.Classes.Context
+{
+    class UserContext : DbContext
+    {
+        public DbSet<User> Clubs { get; set; }
+        public UserContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(Config.ConnectionConfig, Config.Version);
+    }
+}
