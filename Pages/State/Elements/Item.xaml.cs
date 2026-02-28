6971547e19121173c9f4e7c016ab3394ab168272
ANosok <kirill60278@gmail.com>
2026-02-28 14:25:05 +0500

    Разработанная страница State. Cабитов

---
+++ Pages/State/Elements/Item.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VinylRecordsApplication_Sabitov.Pages.State.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        public Item()
+        {
+            InitializeComponent();
+        }
+    }
+}
