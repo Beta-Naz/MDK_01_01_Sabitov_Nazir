5eb72ebe6ed21d1171ef46bf6c697b714991ffa3
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-23 09:49:58 +0500

    Разработанные контексты данных. Сабитов

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatStudents_Sabitov.Classes.Common;
+using ChatStudents_Sabitov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Sabitov.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.config);
+        }
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,17 @@
+﻿using ChatStudents_Sabitov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Sabitov.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext() =>
+            Database.EnsureCreated();
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=users.db");
+        }
+    }
+}
