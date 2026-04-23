3b7a21ae201999b8995718906ccf9c86c5741ed7
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-23 12:22:25 +0500

    Разработанный пользовательский элемент Message. Sabitov

---
+++ Pages/Items/ItMessage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using ChatStudents_Sabitov.Classes.Common;
+using ChatStudents_Sabitov.Models;
+
+namespace ChatStudents_Sabitov.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для ItMessage.xaml
+    /// </summary>
+    public partial class ItMessage : UserControl
+    {
+        public ItMessage(Message message, User UserFrom)
+        {
+            InitializeComponent();
+            if(UserFrom == null)
+            {
+                return;
+            }
+            if (UserFrom.Photo != null)
+            {
+                imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            }
+            FIO.Content = UserFrom?.ToFIO();
+            tbMessage.Text = message.ContentMessage;
+
+        }
+    }
+}
