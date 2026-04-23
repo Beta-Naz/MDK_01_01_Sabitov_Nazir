c3d5a5bd42927f77af41f451bd3ea19f5d89538f
Kik02 <Golovin.Savelii@yandex.ru>
2026-04-23 09:39:36 +0500

    Разработанные общие классы. Sabitov

---
+++ Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Sabitov.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] data)
+        {
+            BitmapImage image = new();
+            using (var stream = new MemoryStream(data))
+            {
+                stream.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = stream;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatStudents_Sabitov.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=;Database=;User=;Pwd=";
+    }
+}
