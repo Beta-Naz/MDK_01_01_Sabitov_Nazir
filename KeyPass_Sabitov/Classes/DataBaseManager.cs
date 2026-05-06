e90d6fb73ad142c4463f0860ecbcf15972baa75c
Beta-Naz <sabitovnazir6@gmail.com>
2026-05-06 23:41:27 +0500

    Разработанные вспомогательные классы для сервера. Сабитов

---
+++ KeyPass_Sabitov/Classes/DataBaseManager.cs
@@ -0,0 +1,19 @@
+﻿using KeyPass_Sabitov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Sabitov.Classes
+{
+    public class DataBaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DataBaseManager() => 
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;uid=student;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ KeyPass_Sabitov/Classes/JwtToken.cs
@@ -0,0 +1,53 @@
+﻿using KeyPass_Sabitov.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace KeyPass_Sabitov.Classes
+{
+    public class JwtToken
+    {
+        static byte[] Key = Encoding.UTF8.GetBytes("Beta");
+        public static string Generate(User user)
+        {
+            JwtSecurityTokenHandler tokenHandler = new ();
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("UserId", user.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Key),
+                    SecurityAlgorithms.HmacSha256Signature
+                 )
+            };
+            SecurityToken Token = tokenHandler.CreateToken (tokenDescriptor);
+            return tokenHandler.WriteToken(Token);
+        }
+        public static int? GetUserIdFromToken(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+                TokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+                JwtSecurityToken jwtSecurity = (JwtSecurityToken)validatedToken;
+                string userId = jwtSecurity.Claims.First(x => x.Type == "UserId").Value;
+                return int.Parse(userId);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
