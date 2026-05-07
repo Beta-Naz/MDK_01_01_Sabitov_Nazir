a4ea226838196048a188b77684af43b69ca60f25
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-07 11:18:39 +0500

    Проверка. Сабитов

--- KeyPass_Sabitov/Classes/DataBaseManager.cs
+++ KeyPass_Sabitov/Classes/DataBaseManager.cs
@@ -5,6 +5,10 @@ namespace KeyPass_Sabitov.Classes
 {
     public class DataBaseManager : DbContext
     {
+        private static readonly string _sourse = "Server=10.0.201.112;" +
+            "Database=base1_ISP_23_1_21;" +
+            "User Id=ISP_23_1_21;" +
+            "Password=D7x7gZZp-3_;";
         public DbSet<Storage> Storages { get; set; }
         public DbSet<User> Users { get; set; }
         public DataBaseManager() => 
@@ -12,7 +16,7 @@ namespace KeyPass_Sabitov.Classes
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(
-                "server=127.0.0.1;uid=student;pwd=;database=Storage;",
+                _sourse,
                 new MySqlServerVersion(new Version(8, 0, 11)));
         }
     }
--- KeyPass_Sabitov/Program.cs
+++ KeyPass_Sabitov/Program.cs
@@ -7,18 +7,31 @@ option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
         Version = "v1",
         Title = "Инструментарий"
     });
-    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeePass.xml");
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeyPass_Sabitov.xml");
     option.IncludeXmlComments(PathFile);
 });
 var app = builder.Build();
 app.UseSwagger();
 app.UseRouting();
+
 app.UseEndpoints(endpoints =>
 {
     _ = endpoints.MapControllers();
 });
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
+
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
 });
+
 app.Run();
