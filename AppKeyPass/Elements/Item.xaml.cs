cafe194336de8cc1a246bd6d331af337233819db
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-07 12:12:21 +0500

    Разработанный пользовательский элемент для клиента. Сабитов

---
+++ AppKeyPass/Elements/Item.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using AppKeyPass.Models;
+using AppKeyPass.Pages;
+
+namespace AppKeyPass.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        private Storage _storage;
+        private Main _currentMain;
+        public Item(Storage storage, Main main)
+        {
+            InitializeComponent();
+            _storage = storage;
+            _currentMain = main;
+            if(_storage != null)
+            {
+                tbLogin.Text = _storage.Login;
+                tbName.Text = _storage.Name;    
+                tbPassword.Text = _storage.Password;
+                tbUrl.Text = _storage.Url;
+            }
+        }
+
+        private void Update(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainWindow.Instance.OpenPages(new Pages.Add(_storage));
+        }
+
+        private void Delete(object sender, System.Windows.RoutedEventArgs e)
+        {
+            StorageContext.Delete(_storage.Id);
+            _currentMain.sPStorageList.Children.Remove(this);
+            MessageBox.Show("Данные удалены");
+        }
+    }
+}
--- AppKeyPass/MainWindow.xaml.cs
+++ AppKeyPass/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AppKeyPass
 {
@@ -16,9 +8,17 @@ namespace AppKeyPass
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Instance { get; private set; }
+        public static string Token;
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+            OpenPages(new Pages.Login());
+        }
+        public void OpenPages(Page openPage)
+        {
+            frame.Navigate(openPage);
         }
     }
 }
